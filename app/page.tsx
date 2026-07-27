55a5befe1750eafc9386ce237e492ce74ad04864
Ado Manyange <adomanyange8@gmail.com>
2026-07-27 12:43:02 -0500

    updated hero

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Band263Website() {
         className="relative h-screen flex items-center justify-center text-center bg-cover bg-center"
         style={{
           backgroundImage:
-            "url('/New Hero page1.png')",
+            "url('/hero.png')",
         }}
       >
         <div className="absolute inset-0 bg-black/70"></div>
